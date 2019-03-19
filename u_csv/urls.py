2f59ab23665aacb132c1ecc7a0fa5958eeec6654
Abdurasul <rakhimovabdurasul@gmail.com>
2019-03-19 12:09:23 +0300

    Fixed upload bug and small feature

--- u_csv/urls.py
+++ u_csv/urls.py
@@ -7,7 +7,7 @@ from django.conf.urls.static import static
 urlpatterns = [
 
     path('', views.model_form_upload, name='upload'),
-    path('csv/', views.print_csv_file, name='print_csv_file'),
+    #path('csv/', views.print_csv_file, name='print_csv_file'),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.BASE_DIR)
--- u_csv/views.py
+++ u_csv/views.py
@@ -11,28 +11,33 @@ from CATt import settings
 import json
 
 def model_form_upload(request):
+    #form = DocumentForm()
     if request.method == 'POST':
-        form = DocumentForm(request.POST, request.FILES)
-        if form.is_valid():
-            form.save()
-            return redirect('csv/')
+        save_path = os.path.join(settings.BASE_DIR, 'files/')
+        path = default_storage.save(save_path, request.FILES['file'])
+        try:
+            ans = str(Data.getScheduleData(path)).split(nl)
+        except:
+            ans = list(["Something went wrong. Choose another file"])
+        #data = json.dumps(ans)
+        #return HttpResponse(data, content_type="application/json")
+        return render(request, 'upload_form.html', { 'schedule': ans })
     else:
-        form = DocumentForm()
-    return render(request, 'upload_form.html')
+        #form = DocumentForm()
+        return render(request, 'upload_form.html')
 
 
 def print_csv_file(request):
-    print(request.FILES)
-
-
-    #save_path = os.path.join(settings.BASE_DIR, 'files/')
-    #save_path = os.path.join(save_path, request.FILES['file'])
-    # path = os.path.join(settings.BASE_DIR, 'files/')
-    path = 'Backend/Sample Data.csv'
-    print(path)
-    #path = default_storage.save(save_path, request.FILES['file'])
-    ans = str(Data.getScheduleData(path)).split(nl)
-    data = json.dumps(ans)
-    return HttpResponse(data, content_type="application/json")
-    #return render(request, 'print_csv.html', { 'ans': ans })
+    if request.method == 'POST':
+        save_path = os.path.join(settings.BASE_DIR, 'files/')
+        path = default_storage.save(save_path, request.FILES['file'])
+        try:
+            ans = str(Data.getScheduleData(path)).split(nl)
+        except:
+            ans = "Something went wrong. Choose another file"
+        #data = json.dumps(ans)
+        #return HttpResponse(data, content_type="application/json")
+        return render(request, 'upload_form.html', { 'schedule': ans })
+    else:
+        redirect('')        
 
